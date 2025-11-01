e55d5b171920e7ad8c705a27f8da2b081ac2b117
Josh Day <emailjoshday@gmail.com>
2025-11-01 19:48:36 -0400

    remove download from CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,20 @@ using Landfire, Extents, Test
 @testset "Landfire.jl" begin
     @test Landfire.healthcheck().success
 
+    Landfire._update_products!!()
+
     @test length(Landfire.products(product_name="Vegetation")) > 0
 
-    @test length(Landfire.products(product_name = "Landfire Map Zones")) == 1
+    @test length(Landfire.products(product_name = "LANDFIRE Map Zones")) == 1
 
-    @testset "Landfire.download" begin
-        ex = Extent(X = (-105.69436f0, -105.052795f0), Y = (39.912888f0, 40.26297f0))
-        prods = Landfire.products(product_name = "13 Anderson Fire Behavior Fuel Models 2022")
-        @test length(prods) == 1
+    # Service is not reliabile enough to run this regularly:
 
-        file = Landfire.download(prods, ex, every=5)
-        @test isfile(file)
-    end
+    # @testset "Landfire.download" begin
+    #     ex = Extent(X = (-105.69436f0, -105.052795f0), Y = (39.912888f0, 40.26297f0))
+    #     prods = Landfire.products(product_name = "13 Anderson Fire Behavior Fuel Models 2022")
+    #     @test length(prods) == 1
+
+    #     file = Landfire.download(prods, ex, every=5)
+    #     @test isfile(file)
+    # end
 end
